921de6a8c8ae3048c091e9b16b201261b9934d86
DEEPAK P <nanideepak05@gmail.com>
2026-01-20 12:47:41 +0530

    smallest is twice of the number

---
+++ smallest.py
@@ -0,0 +1,16 @@
+def smallest(num):
+    res = 0 
+    while(num!=0):
+        rem = num % 10
+        res = rem + res
+        num = num // 10
+    return res
+
+if __name__ == '__main__':
+    n =14
+    n1 =n+1
+    while (smallest(n)*2 != smallest(n1)):
+        n1 = n1 +1
+                     
+    print(n1)
+        
